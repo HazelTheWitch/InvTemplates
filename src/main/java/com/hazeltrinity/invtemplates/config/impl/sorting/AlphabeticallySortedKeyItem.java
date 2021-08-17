d6b9b39e3fc517416054b5a8cd249ceb1e21dcb2
Hazel Rella <hazelrella11@gmail.com>
2021-08-17 11:33:16 -0700

    Sort by quantity after alphabetically

--- src/main/java/com/hazeltrinity/invtemplates/config/impl/sorting/AlphabeticallySortedKeyItem.java
+++ src/main/java/com/hazeltrinity/invtemplates/config/impl/sorting/AlphabeticallySortedKeyItem.java
@@ -1,10 +1,16 @@
 package com.hazeltrinity.invtemplates.config.impl.sorting;
 
+import com.google.gson.annotations.Expose;
+import com.hazeltrinity.invtemplates.InvTemplates;
 import com.hazeltrinity.invtemplates.config.KeyItem;
 import com.hazeltrinity.invtemplates.config.SortingKey;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class AlphabeticallySortedKeyItem extends KeyItem {
+    @Expose(serialize = false, deserialize = false)
+    private static final String DIGIT_STRING = "9876543210";
+
     public AlphabeticallySortedKeyItem() {
         super("alphabetically-sorting");
     }
@@ -18,7 +24,26 @@ public class AlphabeticallySortedKeyItem extends KeyItem {
         return new SortingKey<>();
     }
 
+    @Override
     public SortingKey<String> valueOf(ItemStack stack) {
-        return new SortingKey<>(stack.getName().getString());
+        return new SortingKey<>(stack.getName().getString(), constructQuantityString(stack.getCount(), stack.getMaxCount()));
+    }
+
+    private String constructQuantityString(int quantity, int maxQuantity) {
+        StringBuilder builder = new StringBuilder();
+
+        int digit;
+
+        while (maxQuantity > 0) {
+            digit = quantity % 10;
+            quantity /= 10;
+            maxQuantity /= 10;
+
+            builder.append(DIGIT_STRING.charAt(digit));
+        }
+
+        builder.reverse();
+
+        return builder.toString();
     }
 }
